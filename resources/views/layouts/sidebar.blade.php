2cf0acc9064d404e4b22478914d2322cecf516b0
Wendell Suazo <wendellchansuazo11@gmail.com>
2020-12-07 21:23:19 +0800

    fix admin-logo

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,9 +1,9 @@
 <aside class="main-sidebar sidebar-light-success elevation-4">
-    <a href="/" class="brand-link">
-        <img src="https://infyom.com/images/logo/blue_logo_150x150.jpg"
-             alt="AdminLTE Logo"
+    <a href="/" class="brand-link d-flex align-items-center">
+        <img src="{{ asset('img/admin.png') }}"
+             alt="logo"
              class="brand-image img-circle elevation-3">
-        <span class="brand-text font-weight-light">{{ config('app.name') }}</span>
+        <span class="brand-text font-weight-light"><img src="{{ asset('img/logo-dark.png') }}" alt=""></span>
     </a>
 
     <div class="sidebar">
