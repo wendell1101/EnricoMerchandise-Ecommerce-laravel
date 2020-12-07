21c7880f796a86ca73e6abb3a4b064693791b4e4
Wendell Suazo <wendellchansuazo11@gmail.com>
2020-12-07 21:40:31 +0800

    add adminseeder

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -19,5 +19,17 @@ class UserSeeder extends Seeder
             'role' => 'admin',
             'password' => Hash::make('admin'),
         ]);
+        User::create([
+            'name' => 'wendell2 suazo',
+            'email' => 'admin2@gmail.com',
+            'role' => 'admin',
+            'password' => Hash::make('admin'),
+        ]);
+        User::create([
+            'name' => 'wendell3 suazo',
+            'email' => 'admin3@gmail.com',
+            'role' => 'admin',
+            'password' => Hash::make('admin'),
+        ]);
     }
 }
