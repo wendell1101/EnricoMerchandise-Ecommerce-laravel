f69f9ab0b12d8cf9b04debdadc5e754f8912317d
Wendell Suazo <wendellchansuazo11@gmail.com>
2020-12-08 15:02:10 +0800

    edit google icon

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,96 +1,93 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>{{ config('app.name') }}</title>
+    <title>Enrico Merchandise| Login</title>
 
     <!-- Tell the browser to be responsive to screen width -->
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
 
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css"
-          integrity="sha512-1PKOgIY59xJ8Co8+NE6FZ+LOAZKjy+KY8iq0G4B3CyeY6wYHN3yt9PW0XpSriVlkMXe40PTKnXrLnZ9+fkDaog=="
-          crossorigin="anonymous"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css" integrity="sha512-1PKOgIY59xJ8Co8+NE6FZ+LOAZKjy+KY8iq0G4B3CyeY6wYHN3yt9PW0XpSriVlkMXe40PTKnXrLnZ9+fkDaog==" crossorigin="anonymous" />
 
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
 </head>
+
 <body class="hold-transition login-page">
-<div class="login-box">
-    <div class="login-logo">
-        <a href="{{ url('/home') }}"><img src="{{ asset('img/logo-dark.png')}}" alt="logo" style="width:200px"></a>
-    </div>
-    <!-- /.login-logo -->
-
-    <!-- /.login-box-body -->
-    <div class="card">
-        <div class="card-body login-card-body">
-            <p class="login-box-msg">Login with</p>
-            <a href="{{ url('auth/google') }}" class="btn btn-light text-danger border-info btn-block">Google</a>
-            <p class="login-box-msg">Or </p>
-
-            <form method="post" action="{{ url('/login') }}">
-                @csrf
-
-                <div class="input-group mb-3">
-                    <input type="email"
-                           name="email"
-                           value="{{ old('email') }}"
-                           placeholder="Email"
-                           class="form-control @error('email') is-invalid @enderror">
-                    <div class="input-group-append">
-                        <div class="input-group-text"><span class="fas fa-envelope"></span></div>
-                    </div>
-                    @error('email')
-                    <span class="error invalid-feedback">{{ $message }}</span>
-                    @enderror
-                </div>
-
-                <div class="input-group mb-3">
-                    <input type="password"
-                           name="password"
-                           placeholder="Password"
-                           class="form-control @error('password') is-invalid @enderror">
-                    <div class="input-group-append">
-                        <div class="input-group-text">
-                            <span class="fas fa-lock"></span>
-                        </div>
-                    </div>
-                    @error('password')
-                    <span class="error invalid-feedback">{{ $message }}</span>
-                    @enderror
-
-                </div>
-
-                <div class="row">
-                    <div class="col-8">
-                        <div class="icheck-primary">
-                            <input type="checkbox" id="remember">
-                            <label for="remember">Remember Me</label>
-                        </div>
-                    </div>
-
-                    <div class="col-4">
-                        <button type="submit" class="btn btn-primary btn-block">Sign In</button>
-                    </div>
-
-                </div>
-            </form>
-
-            <p class="mb-1">
-                <a href="{{ route('password.request') }}">Forgot password?</a>
-            </p>
-            <p class="mb-0">
-                <span>Not yet registered? </span><a href="{{ route('register') }}" class="text-center">Register here</a>
-            </p>
+    <div class="login-box">
+        <div class="login-logo">
+            <a href="{{ url('/home') }}"><img src="{{ asset('img/logo-dark.png')}}" alt="logo" style="width:200px"></a>
         </div>
-        <!-- /.login-card-body -->
+        <!-- /.login-logo -->
+
+        <!-- /.login-box-body -->
+        <div class="card">
+            <div class="card-body login-card-body">
+                <p class="login-box-msg">Login Here</p>
+
+                <a href="{{ url('auth/google') }}" class="border-info form-control text-center  text-info p-2">
+                <i class="fab fa-google"></i> Login with Google
+                </a>
+                <p class="login-box-msg mt-2">Or </p>
+
+                <form method="post" action="{{ url('/login') }}">
+                    @csrf
+
+                    <div class="input-group mb-3">
+                        <input type="email" name="email" value="{{ old('email') }}" placeholder="Email" class="form-control @error('email') is-invalid @enderror">
+                        <div class="input-group-append">
+                            <div class="input-group-text"><span class="fas fa-envelope"></span></div>
+                        </div>
+                        @error('email')
+                        <span class="error invalid-feedback">{{ $message }}</span>
+                        @enderror
+                    </div>
+
+                    <div class="input-group mb-3">
+                        <input type="password" name="password" placeholder="Password" class="form-control @error('password') is-invalid @enderror">
+                        <div class="input-group-append">
+                            <div class="input-group-text">
+                                <span class="fas fa-lock"></span>
+                            </div>
+                        </div>
+                        @error('password')
+                        <span class="error invalid-feedback">{{ $message }}</span>
+                        @enderror
+
+                    </div>
+
+                    <div class="row">
+                        <div class="col-8">
+                            <div class="icheck-primary">
+                                <input type="checkbox" id="remember">
+                                <label for="remember">Remember Me</label>
+                            </div>
+                        </div>
+
+                        <div class="col-4">
+                            <button type="submit" class="btn btn-primary btn-block">Sign In</button>
+                        </div>
+
+                    </div>
+                </form>
+
+                <p class="mb-1">
+                    <a href="{{ route('password.request') }}">Forgot password?</a>
+                </p>
+                <p class="mb-0">
+                    <span>Not yet registered? </span><a href="{{ route('register') }}" class="text-center">Register here</a>
+                </p>
+            </div>
+            <!-- /.login-card-body -->
+        </div>
+
     </div>
+    <!-- /.login-box -->
 
-</div>
-<!-- /.login-box -->
-
-<script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('js/app.js') }}" defer></script>
 
 </body>
+
 </html>
